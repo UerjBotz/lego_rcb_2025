bf972a2db4113777f7c1129eeccffbdf2f82bcd5
HPele21 <21humbertopelegrino@gmail.com>
2024-11-01 14:52:32 -0300

    teste de usar giro no DriveBase: negativo

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import mao
 from polyfill import Enum
 
 ID = Enum("ID", ["MAO", "CABECA", "TESTE"])
-id = ID.CABECA
+id = ID.TESTE
 
 if id == ID.CABECA:
     hub = cabeca.setup()
@@ -18,8 +18,36 @@ elif id == ID.MAO:
     hub.speaker.beep(frequency=500, duration=100)
     mao.main()
     hub.speaker.beep(frequency=250, duration=200)
+
 elif id == ID.TESTE:
-    ...
+    from pybricks.hubs import PrimeHub
+    from pybricks.pupdevices import Motor, ColorSensor, UltrasonicSensor
+
+    from pybricks.parameters import Port, Axis, Stop, Direction, Button, Color
+    from pybricks.tools      import wait, StopWatch
+    from pybricks.robotics   import DriveBase
+    
+    #hub = PrimeHub()
+    hub = PrimeHub(top_side=Axis.X, front_side=-Axis.Z) #! sinais dos eixos
+    #rodas.use_gyro(True) #! testar depois, funcionou pior/inconsistente
+
+    roda_esq   = Motor(Port.B, positive_direction=Direction.COUNTERCLOCKWISE)
+    roda_dir   = Motor(Port.A, positive_direction=Direction.CLOCKWISE)
+
+    rodas = DriveBase(roda_esq, roda_dir,
+                      wheel_diameter=88, axle_track=145.5) #! ver depois se recalibrar
+    
+    #battery_voltage = hub.battery.voltage()
+    #print(battery_voltage)
+
+    rodas.straight(300)
+    rodas.turn(360, then=Stop.BRAKE, wait=True)
+    #rodas.turn(360, then=Stop.BRAKE, wait=False)
+    #while not rodas.done():
+    #    print(hub.imu.heading())
+    
+    # while True:
+    #    print(hub.imu.tilt())
+
 else:
     assert False
-
