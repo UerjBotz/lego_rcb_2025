fc292ac3f449114f0ff23f4c02c2e44d1b3c4bc2
pgvalle <pgvalle.nogueira@gmail.com>
2025-08-21 15:58:09 -0300

    garra """"funcionando""""

--- garra.py
+++ garra.py
@@ -1,6 +1,8 @@
 from pybricks.parameters import Port, Stop
 from pybricks.tools      import wait
 
+DIST_GARRA = 600
+
 def fecha_garra(motor_garra, vel=240):
     motor_garra.run_until_stalled(vel, then=Stop.COAST, duty_limit=None)
     motor_garra.run_angle(vel, 30, then=Stop.COAST, wait=False)
@@ -9,7 +11,7 @@ def abre_garra(motor_garra, vel=240, ang_volta=42):
     motor_garra.run_angle(vel, -ang_volta, then=Stop.COAST, wait=True)
 
 def levanta_garra(motor_vertical, vel=240):
-    motor_vertical.run_until_stalled(vel, then=Stop.COAST, duty_limit=None)
+    motor_vertical.run_angle(vel, DIST_GARRA, then=Stop.COAST, wait=True)
 
 def abaixa_garra(motor_vertical, vel=240):
-    motor_vertical.run_until_stalled(-vel, then=Stop.COAST, duty_limit=None)
+    motor_vertical.run_angle(-vel, DIST_GARRA, then=Stop.COAST, wait=True)
